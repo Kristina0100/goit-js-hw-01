52e90620a4775e0013d1f3a07357a0f6daaa746f
Kristina_Humeniuk <156364109+Kristina0100@users.noreply.github.com>
2024-06-09 20:49:22 +0300

    Change method in 3rd task

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-    const width = ((Number.parseInt(content)) + 2 * (Number.parseInt(padding)) + 2 * (Number.parseFloat(border))); 
+    const width = ((Number.parseFloat(content)) + 2 * (Number.parseFloat(padding)) + 2 * (Number.parseFloat(border))); 
     return width;
 }
 
